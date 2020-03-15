6d1fa46b069e77986c3246ccfe0099495f95885b
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-03-15 22:03:38 +0100

    Show user's name and avatar when logged

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -10,7 +10,7 @@ const isLogged = (req, res, next) => {
 };
 
 router.get('/logged', isLogged, (req, res) => {
-    res.render('logged');
+    res.render('logged', { name: req.user.displayName, photo: req.user.photos[0].value} );
 });
 
 router.get('/no-permission', (req, res) => {
