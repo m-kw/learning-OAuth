d475c3e4d18d74a1b33535bffe60c7616f4f8d2b
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-03-15 12:46:02 +0100

    Prepare server to use Passport

--- server.js
+++ server.js
@@ -2,8 +2,33 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
+const passport = require('passport');
+const GoogleStartegy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
 
 const app = express();
+
+passport.use(new GoogleStartegy({
+  clientID: '7133139195667-i7nit1bktop5jurhvv6vhnpervffked.apps.googleusercontent.com',
+  clientSecret: 'efCFhrRWhdqoA7YeXMypzcaT',
+  callbackURL: 'http://localhost:8000/auth/google/callback',
+}, (accessToken, refreshToken, profile, done) => {
+  done(null, profile);
+}));
+
+app.use(session({ secret: 'anything' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// serialize user when saving to session
+passport.serializeUser((user, serialize) => {
+  serialize(null, user);
+});
+
+// deserialize user when reading from session
+passport.deserializeUser((obj, deserialize) => {
+  deserialize(null, obj);
+});
 
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
