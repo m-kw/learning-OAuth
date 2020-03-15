9e09d996031b7f720b79a0ac67cc29be0b06e759
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-03-15 13:58:18 +0100

    Add profile and profileSettings subpages, add their endpoints and deny access when not logged

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,11 +2,31 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
-    res.render('logged');
+    if (!req.user) {
+        res.redirect('/user/no-permission');
+    } else {
+        res.render('logged');
+    }
 });
 
 router.get('/no-permission', (req, res) => {
     res.render('noPermission');
+});
+
+router.get('/profile', (req, res) => {
+    if (!res.user) {
+        res.redirect('/user/no-permission');
+    } else {
+        res.render('profile');
+    }
+});
+
+router.get('/profile/settings', (req, res) => {
+    if (!req.user) {
+        res.redirect('/user/no-permission');
+    } else {
+        res.render('profileSettings');
+    }
 });
 
 
